16d133e781ada2fad14201b4f10aa14d39e7f4a3
HellenNzisa <hellen.mutemi@strathmore.edu>
2025-04-26 23:38:50 +0300

    Done with even or odd checker

---
+++ evenOrOdd.py
@@ -0,0 +1,7 @@
+def evenOrOddchecker(number):
+    if number % 2 == 0:
+        return "EVEN"
+     #Here I am checking if the number is even or odd using the modulus operator
+    else:
+        return "ODD"
+print(evenOrOddchecker(1678))
---
+++ sumation.py
@@ -0,0 +1,5 @@
+def sumation(numbers):
+    return sum(numbers)
+numbers = [1, 2, 3, 4, 5]
+result = sumation(numbers)
+print(f"The sum of the list is: {result}")
